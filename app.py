3bddbb023cc35260912027d6e3f798f41e91e424
Tomi_Ohl <ohlslager.tom@gmail.com>
2020-04-13 22:00:13 +0200

    Basic html, css, app routing

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from db_actions import exec
 
 app = Flask(__name__)
 
 
 # index.html
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # ha benne van a juzer az adatbazisban
+        return redirect(url_for('profile'))
+    return render_template('index.html')
+
+
+# profile.html
+@app.route('/profile')
+def profile():
     data = exec("SELECT * FROM Countries")
-    return render_template('index.html', colnames=data[0], rows=data[1])
+    return render_template('profile.html', colnames=data[0], rows=data[1])
+
+
+# categpries.html
+@app.route('/categories')
+def categories():
+    return render_template('categories.html')
+
+
+# mostactive.html
+@app.route('/mostactive')
+def mostactive():
+    return render_template('mostactive.html')
+
+
+# worldmap.html
+@app.route('/worldmap')
+def worldmap():
+    return render_template('worldmap.html')
 
 
 if __name__ == "__main__":
